ab7d07fc93d3dd0ef5d9d5bf477720c75abdf155
John Kyalo <jkm.charlore@gmail.com>
2022-11-28 21:34:59 +0300

    demonstrating python indentation

---
+++ python/indentation_example_one.py
@@ -0,0 +1,11 @@
+score=7 
+if score>=0 and score < 20:
+   print('Grade E')
+elif(score >=20 and score <40):
+   print('Grade D')
+elif(score >=40 and score <60):
+   print('Grade C') 
+elif(score >=60 and score <70):
+   print('Grade B') 
+else:
+   print('Grade A')
