f7a04a05785cdbab47cd0346d493fbae7c4c8846
John Kyalo <jkm.charlore@gmail.com>
2022-12-16 12:18:41 +0300

    Python Comments

---
+++ python/python_comments/python_comments.py
@@ -0,0 +1,32 @@
+
+#single line comments
+name='John' #name hold the person's name
+dob='Feb 1,2010' #dob is the date of birth
+#age in years
+age=12
+print(name,' was born on ',dob , ' and is ',age ,' years old.')
+
+
+#multi line comments
+#name hold the person's name
+#dob is the date of birth
+#age in years
+
+name='Jane' 
+dob='July 15,2005' 
+age=17
+print(name,' was born on ',dob , ' and is ',age ,' years old.')
+
+
+
+"""
+multi line comments  
+name hold the person's name
+dob is the date of birth
+age in years
+"""
+
+name='Tom' 
+dob='Nov 20,2018' 
+age=4
+print(name,' was born on ',dob , ' and is ',age ,' years old.')
