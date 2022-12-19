4d22ce5aacfc61a1569b44c7e15104c801ab6464
John Kyalo <jkm.charlore@gmail.com>
2022-12-19 20:57:45 +0300

    python variables

---
+++ python/python_variables/python_variables.py
@@ -0,0 +1,7 @@
+#we just assign value 81 to score,and can use the variable.The type,int in this case will be determined at runtime
+score=81
+print(score)
+#we assign String 'Grade A' to score,it's now of type String
+score="Grade A"
+print(score)
+
